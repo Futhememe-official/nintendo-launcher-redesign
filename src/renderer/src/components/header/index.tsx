f6d5f988573f284cc988788dbf7a14bc193761c9
Futheme <kyoro_souza@hotmail.com>
2025-01-06 18:13:20 -0300

    🎨 refactor Header component to use useNetwork hook and add battery status monitoring

--- src/renderer/src/components/header/index.tsx
+++ src/renderer/src/components/header/index.tsx
@@ -1,35 +1,23 @@
-import { Battery, WifiHigh, WifiLow, WifiOff, WifiZero } from 'lucide-react'
+import {
+  BatteryCharging,
+  BatteryFull,
+  BatteryLow,
+  BatteryMedium,
+  BatteryWarning,
+  WifiHigh,
+  WifiLow,
+  WifiOff,
+  WifiZero
+} from 'lucide-react'
 
 import { batteryRow, headerStyle, systemIcons, wifiIcon, wifiOff } from './styles'
-import { useSyncExternalStore } from 'react'
-import { NetworkType } from './types'
-
-function subscribe(callback: () => void) {
-  // @ts-ignore connection exists
-  navigator?.connection?.addEventListener('change', callback)
-  window.addEventListener('online', callback)
-  window.addEventListener('offline', callback)
-  return (): void => {
-    // @ts-ignore connection exists
-    navigator?.connection?.removeEventListener('change', callback)
-    window.removeEventListener('online', callback)
-    window.removeEventListener('offline', callback)
-  }
-}
-
-function getSnapshot(): NetworkType {
-  const isOnline = window.navigator.onLine
-
-  if (!isOnline) {
-    return 'offline'
-  }
-
-  // @ts-ignore connection exists
-  return navigator?.connection?.effectiveType
-}
+import { useNetwork } from './hooks/useNetwork'
+import { useEffect, useMemo, useState } from 'react'
 
 export const Header = (): JSX.Element => {
-  const network = useSyncExternalStore(subscribe, getSnapshot)
+  const network = useNetwork()
+  const [battery, setBattery] = useState<number>(0)
+  const [isCharging, setIsCharging] = useState<boolean>(false)
 
   const wifiIcons = {
     '4g': <WifiHigh className={wifiIcon} />,
@@ -38,6 +26,30 @@ export const Header = (): JSX.Element => {
     offline: <WifiOff className={wifiOff} size={16} />
   }
 
+  useEffect(() => {
+    // @ts-ignore battery exists
+    navigator.getBattery().then((battery) => {
+      setBattery(battery.level)
+      setIsCharging(battery.charging)
+
+      battery.addEventListener('chargingchange', () => {
+        setIsCharging(battery.charging)
+      })
+
+      battery.addEventListener('onlevelchange', () => {
+        setBattery(battery.level)
+      })
+    })
+  }, [])
+
+  const batteryLevel = useMemo(() => {
+    if (battery <= 0) {
+      return Number(String(battery).split('.')[1])
+    }
+
+    return battery * 100
+  }, [battery])
+
   return (
     <header className={headerStyle}>
       <div>primeiro</div>
@@ -45,8 +57,17 @@ export const Header = (): JSX.Element => {
         <p>12:34</p>
         {wifiIcons[network]}
         <div className={batteryRow}>
-          <p>34%</p>
-          <Battery />
+          <p>{batteryLevel}%</p>
+          {isCharging ? (
+            <BatteryCharging />
+          ) : (
+            <>
+              {batteryLevel < 10 && <BatteryWarning />}
+              {batteryLevel < 25 && batteryLevel > 10 && <BatteryLow />}
+              {batteryLevel < 75 && batteryLevel > 25 && <BatteryMedium />}
+              {batteryLevel > 75 && <BatteryFull />}
+            </>
+          )}
         </div>
       </div>
     </header>
