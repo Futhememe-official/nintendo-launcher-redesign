09ec88755203401783bcb7c4e87a576b235ed90b
Futheme <kyoro_souza@hotmail.com>
2025-01-06 13:47:47 -0300

    ✨ add Main component with styled layout configuration

---
+++ src/renderer/src/components/layout/main.tsx
@@ -0,0 +1,15 @@
+import { styled } from '@/styled-system/jsx'
+
+export const Main = styled('main', {
+  base: {
+    display: 'flex',
+    flexDir: 'column',
+    minHeight: '100vh',
+    maxHeight: '100vh',
+    backgroundColor: 'background',
+    position: 'relative',
+    padding: '36px 16px 16px 16px',
+
+    color: 'text'
+  }
+})
