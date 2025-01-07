4dcbda7f3921cf502ff8c526e82617e85d8cf124
Futheme <kyoro_souza@hotmail.com>
2025-01-06 23:50:54 -0300

    🚧 implement GameCard component and integrate it into CarrouselSection

--- src/renderer/src/components/carrousel-section/index.tsx
+++ src/renderer/src/components/carrousel-section/index.tsx
@@ -1,5 +1,23 @@
+import { Carousel, CarouselContent, CarouselItem } from '../carousel'
+import { GameCard } from '../game-card'
 import { sectionStyle } from './styles'
 
 export const CarrouselSection = (): JSX.Element => {
-  return <section className={sectionStyle}>l</section>
+  return (
+    <section className={sectionStyle}>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <GameCard />
+          </CarouselItem>
+          <CarouselItem>
+            <GameCard />
+          </CarouselItem>
+          <CarouselItem>
+            <GameCard />
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+    </section>
+  )
 }
---
+++ src/renderer/src/components/game-card/index.tsx
@@ -0,0 +1,8 @@
+export const GameCard = (): JSX.Element => {
+  return (
+    <div>
+      <p>Title</p>
+      <p>Clique aqui</p>
+    </div>
+  )
+}
