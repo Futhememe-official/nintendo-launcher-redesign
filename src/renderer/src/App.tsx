379299a58358f2fbafccd95c3e729551611333c6
Gustavo S Souza <kyoro_souza@hotmail.com>
2025-01-18 00:16:04 -0300

    ✨ refactor App component to use cx for drag region styling

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,21 +1,21 @@
 import { CarrouselSection } from './components/carrousel-section'
 import { Header } from './components/header'
 import { Main } from './components/layout/main'
-import { css } from '@/styled-system/css'
+import { css, cx } from '@/styled-system/css'
+
+const dragRegion = css({
+  display: 'flex',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  w: '100%',
+  height: '36px'
+})
 
 function App(): JSX.Element {
   return (
     <Main>
-      <div
-        className={css({
-          display: 'flex',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          w: '100%',
-          height: '36px'
-        })}
-      />
+      <div className={cx(dragRegion, 'region-drag')} />
       <Header />
       <CarrouselSection />
     </Main>
